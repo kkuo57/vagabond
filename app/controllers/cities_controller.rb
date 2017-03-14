d8c015c7c7647b45eacceb9f42273a1d8d15e0a0
zeroCalm <smith.timothyh@gmail.com>
2017-03-14 14:08:18 -0700

    adds city show pages, actions to cities controller, routes as resources, bootstrap enabled

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,4 +2,28 @@ class CitiesController < ApplicationController
   def index
     @cities = City.all
   end
+
+  def show
+    @city = City.find(params[:id])
+  end
+
+  def edit
+    #sends 'edit city' form
+  end
+
+  def update
+    @city = City.update(city_params)
+  end
+
+  def destroy
+    @city = City.destroy(params[:id])
+  end
+
+
+  private
+
+  def city_params
+    params.require(:city).permit(:city_name, :country)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'cities#index' 
+  root to: 'cities#index'
+
+  resources :cities
+  resources :users
+
 end
