0f19ed1484bd5f5bc299f536eedf3b80ece4f149
Will Kaspar <Will@Aang.local>
2017-03-14 21:08:55 -0700

    created seed file, switched to Materialize

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+City.destroy_all
+Article.destroy_all
+
+cities = [
+  {city_name:"San Francisco, CA", country:"USA"},
+  {city_name:"Tokyo", country:"Japan"},
+  {city_name:"Topeka, KS", country:"USA"},
+  {city_name:"Mogadishu", country:"Somalia"},
+  {city_name:"Gibraltar", country:"Gibraltar"},
+  {city_name:"Guangzhou", country:"China"},
+  {city_name:"Cairo", country:"Egypt"},
+  {city_name:"Pawnee, IN", country:"USA"},
+  {city_name:"Reykjavik", country:"Iceland"},
+  {city_name:"Christchurch", country:"New Zealand"},
+]
+
+City.create(cities)
