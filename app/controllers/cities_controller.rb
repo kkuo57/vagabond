8b7dbff790ad855ffdbfce6dfdf6d2bb42bde362
Will Kaspar <Will@Aang.local>
2017-03-14 23:06:05 -0700

    fix some login issues, build the BELLErousel

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,20 +1,26 @@
 class CitiesController < ApplicationController
+
+  # Index of All Cities
   def index
     @cities = City.all
   end
 
+  # Show One City Page
   def show
     @city = City.find(params[:id])
   end
 
+  # 'Edit' City
   def edit
     #sends 'edit city' form
   end
 
+  # Updates Information About A City
   def update
     @city = City.update(city_params)
   end
 
+  # Deletes a City
   def destroy
     @city = City.destroy(params[:id])
   end
@@ -22,6 +28,7 @@ class CitiesController < ApplicationController
 
   private
 
+  # Defines parameters for 'City'
   def city_params
     params.require(:city).permit(:city_name, :country)
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,24 @@
 class SessionsController < ApplicationController
+
+  # Login route?
   def new
     @user = User.new
   end
 
+  # Create a new user session
   def create
     @user = User.confirm(user_params)
     if @user
       login(@user)
-      flash[:notice] = "Good job!"
-      redirect_to user_path(@user)
+      flash[:blank] = "Now, you're vagabound!"
+      redirect_to user_path(current_user)
     else
-      flash[:warning] = ":( Try again..."
+      flash[:warning] = "Try again..."
       redirect_to login_path
     end
   end
 
+  # Destroy A User Session
   def destroy
     logout
     flash[:notice] = "Logged Out"
@@ -23,6 +27,7 @@ class SessionsController < ApplicationController
 
   private
 
+  # Define login parameters
   def user_params
     params.require(:user).permit(:email, :password)
   end
