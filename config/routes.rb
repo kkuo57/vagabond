e6ab9572088a7c5ca2352d9bd49c4e750eadfb6e
kkuo57 <kkuo507@gmail.com>
2017-03-17 13:59:22 -0700

    adds edit feature to articles

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get '/articles', to: 'articles#index', as: 'user_articles'
   post '/articles', to: 'articles#create'
   delete '/articles/:id', to: 'articles#destroy'
+  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  put '/articles/:id', to: 'articles#update'
+  patch '/articles/:id', to: 'articles#update'
 
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
